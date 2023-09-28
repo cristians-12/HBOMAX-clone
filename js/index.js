4397bf726972fc4ed1524359eb3733e120a96788
Cristian <97061228+cristians-12@users.noreply.github.com>
2023-09-28 13:52:19 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const observador = new IntersectionObserver((entradas)=>{
     entradas.forEach((e)=>{
         if(e.isIntersecting){
             e.target.classList.add('mostrar');
-            console.log("hola")
+
         }else{
             // e.target.classList.remove('mostrar');
         }
@@ -14,3 +14,25 @@ const oculto = document.querySelectorAll(".hidden");
 oculto.forEach((e)=>(
     observador.observe(e)
 ))
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
